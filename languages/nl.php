e761275a9bedc25f73d9b833dc6fa5873eda2dd2
Jerome Bakker <jeabakker@coldtrick.com>
2017-01-11 12:17:51 +0100

    chore: updated Dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,10 +1,14 @@
 <?php
 /**
  * This file was created by Translation Editor v5.2
- * On 2017-01-04 15:54
+ * On 2017-01-11 12:17
  */
 
 return array (
+  'advanced_comments:settings:defaults' => 'Standaard reactie instellingen',
+  'advanced_comments:settings:defaults:auto_load:help' => 'laad automatisch de volgende serie reacties in als de gebruiker het einde van de pagina bereikt',
+  'advanced_comments:settings:defaults:user_preference' => 'Mogen gebruikers de reactie instellingen aanpassen',
+  'advanced_comments:settings:helper' => 'Formulier ondersteuning',
   'advanced_comments:comment:logged_out' => 'Reageren is alleen toegestaan voor aangemelde gebruikers',
   'advanced_comments:settings:show_login_form' => 'Toon aanmeldformulier voor afgemelde gebruikers onder de reacties',
   'advanced_comments' => 'Geadvanceerde reacties',
