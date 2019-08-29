f6ec5c1a39d7f70f1f61c07586aae72668cbfa77
Jerome Bakker <jeabakker@coldtrick.com>
2019-08-29 09:58:13 +0200

    chore: reorder bootstrap

--- classes/ColdTrick/AdvancedComments/Bootstrap.php
+++ classes/ColdTrick/AdvancedComments/Bootstrap.php
@@ -13,22 +13,32 @@ class Bootstrap extends DefaultPluginBootstrap {
 		// extend css
 		elgg_extend_view('css/elgg', 'css/advanced_comments.css');
 		
-		elgg_extend_view('page/elements/comments', 'advanced_comments/logged_out_notice', 400);
-		
+		// extend views
 		elgg_extend_view('page/components/list', 'advanced_comments/header', 400);
 		elgg_extend_view('page/components/list', 'advanced_comments/loader', 600);
+		elgg_extend_view('page/elements/comments', 'advanced_comments/logged_out_notice', 400);
 		
-		
-		elgg_register_plugin_hook_handler('config', 'comments_latest_first', '\ColdTrick\AdvancedComments\Comments::getCommentsLatestFirst');
-		elgg_register_plugin_hook_handler('config', 'comments_per_page', '\ColdTrick\AdvancedComments\Comments::getCommentsPerPage');
-		elgg_register_plugin_hook_handler('view', 'page/elements/comments', '\ColdTrick\AdvancedComments\Views::untrackCommentsEntity');
-		elgg_register_plugin_hook_handler('view_vars', 'page/components/list', '\ColdTrick\AdvancedComments\Views::checkCommentsListing');
-		elgg_register_plugin_hook_handler('view_vars', 'page/elements/comments', '\ColdTrick\AdvancedComments\Views::trackCommentsEntity');
-		
-		
+		// register ajax views
 		elgg_register_ajax_view('advanced_comments/comments');
 		
 		// register plugin hooks
-// 		elgg_register_plugin_hook_handler('route', 'comment', '\ColdTrick\AdvancedComments\Comments::route');
+		$this->registerHooks();
+	}
+	
+	/**
+	 * Register plugin hook handlers
+	 *
+	 * @return void
+	 */
+	protected function registerHooks() {
+		$hooks = $this->elgg()->hooks;
+		
+		$hooks->registerHandler('config', 'comments_latest_first', __NAMESPACE__ . '\Comments::getCommentsLatestFirst');
+		$hooks->registerHandler('config', 'comments_per_page', __NAMESPACE__ . '\Comments::getCommentsPerPage');
+		$hooks->registerHandler('view', 'page/elements/comments', __NAMESPACE__ . '\Views::untrackCommentsEntity');
+		$hooks->registerHandler('view_vars', 'page/components/list', __NAMESPACE__ . '\Views::checkCommentsListing');
+		$hooks->registerHandler('view_vars', 'page/elements/comments', __NAMESPACE__ . '\Views::trackCommentsEntity');
+		
+// 		$hooks->registerHandler('route', 'comment', __NAMESPACE__ . '\Comments::route');
 	}
 }
