fcf91ee7798af59c7996d82aa1c714c7e5549d4c
Dennis Ploeger <github.dennis@dieploegers.de>
2019-10-27 12:50:36 +0100

    Added german translation
    Added german translation file

---
+++ languages/de.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+  'advanced_comments' => "Erweiterte Kommentarfunktionen",
+	
+	'advanced_comments:header:order' => "Kommentarsortierung",
+	'advanced_comments:header:order:asc' => "Älteste zuerst",
+	'advanced_comments:header:order:desc' => "Neuste zuerst",
+	
+	'advanced_comments:header:limit' => "Maximale Anzahl",
+	'advanced_comments:header:auto_load' => "Automatisch laden",
+	
+	'advanced_comments:comment:logged_out' => "Das Kommentieren ist nur für angemeldete Nutzer erlaubt",
+	
+	'advanced_comments:settings:auto_load:help' => "Automatisch die nächsten Kommentare laden, wenn der Benutezr das Ende der Seite erreicht",
+	'advanced_comments:settings:user_preference' => "Dürfen Benutzer die Kommentareinstellungen verändern?",
+	'advanced_comments:settings:show_login_form' => "Das Anmeldeformular für nicht angemeldete Benutzer unter den Kommentaren anzeigen",
+	'advanced_comments:settings:allow_group_comments' => "Nicht-Mitgliedern einer Gruppe erlauben, etwas in der Gruppe zu kommentieren",
+	'advanced_comments:settings:allow_group_comments:help' => "Wenn ein Benutzer den Inhalt einer Gruppe, von der er nicht Mitglied ist, sehen kann, darf er kommentieren.",
+];
