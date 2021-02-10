983de4673596ff97e77391f14497ca87cbed2254
Dan d'Auge <76149200+danhype@users.noreply.github.com>
2021-02-10 17:13:28 +0100

    Create fr.php
    version 4.0.1

---
+++ languages/fr.php
@@ -0,0 +1,14 @@
+<?php
+
+return array (
+  'advanced_comments:header:order' => 'Ordre des commentaires',
+  'advanced_comments:header:order:asc' => 'Plus anciens en premier',
+  'advanced_comments:header:order:desc' => 'Les derniers en premier',
+  'advanced_comments:header:limit' => 'Limite',
+  'advanced_comments:header:auto_load' => 'Auto-chargement',
+  'advanced_comments' => 'Options avancées pour les commentaires',
+  'advanced_comments:comment:logged_out' => 'Commentaires uniquement disponibles pour les utilisateurs connectés',
+  'advanced_comments:settings:auto_load:help' => 'Charger automatiquement les commentaires suivants lorsque l\'utilisateur atteint la fin de la page',
+  'advanced_comments:settings:user_preference' => 'Les utilisateurs sont-ils autorisés à modifier les paramètres des commentaires',
+  'advanced_comments:settings:show_login_form' => 'Afficher le formulaire de connexion pour les utilisateurs déconnectés',
+);
