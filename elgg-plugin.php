629546e9f1f152309de3da6358b62bde699ee04f
Jerôme Bakker <jeabakker@coldtrick.com>
2021-10-12 15:32:01 +0200

    chore: wrapping up v7.0.1

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -5,7 +5,7 @@ use ColdTrick\AdvancedComments\Controllers\CommentRedirector;
 
 return [
 	'plugin' => [
-		'version' => '7.0',
+		'version' => '7.0.1',
 	],
 	'bootstrap' => Bootstrap::class,
 	'settings' => [
