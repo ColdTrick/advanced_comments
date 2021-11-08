25400507208113ba7638ebd53572c7bdb4e8bc8c
Chris Mitchell <MacMhicheil@users.noreply.github.com>
2021-11-08 07:40:49 +0000

    chore: added Scottish translations

---
+++ languages/gd.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'advanced_comments:settings:show_login_form' => "Seall foirm a\' logaidh a-steach do chleachdaichean nach eil air logadh a-steach.",
+	'advanced_comments:settings:allow_group_comments' => "Leig le cleachdaichean nach eil nam buill beachdan fhàgail air susbaint poblach ann am buidhnean",
+	'advanced_comments:settings:allow_group_comments:help' => "Ma tha seo an comas bidh cleachdaichean comasach air beachdan fhàgail air susbaint poblach ann am buidhnean gun a bhith gabhail ballrachd annta",
+	'advanced_comments:settings:threaded_comments' => "An àireamh de dh'ìrean a tha ceadaichte ann an snàithleanan",
+	'advanced_comments:settings:threaded_comments:help' => "Ma tha seo an comas bidh cleachdaichean comasach air beachdan fhàgail air beachdan eile.",
+	'advanced_comments:settings:threaded_comments:none' => "Chan eil snàithleanan ceadaichte",
+
+	'advanced_comments:comment:logged_out' => "Chan fhaod ach cleachdaichean a th\' air logadh a-steach beachdan fhàgail",
+	'advanced_comments:comment:response_by_author' => "Beachd leis an ùghdar",
+	'advanced_comments:threaded_comment:add' => "Fàg beachd",
+
+];
